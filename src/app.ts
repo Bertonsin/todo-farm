0b75976dfbf90161a4cf0203ce6b806c2b1d771e
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2024-02-23 21:40:21 -0300

    feat: ✨ add eslint config

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
-import {app} from './server'
+import { app } from './server'
 
-app.listen({
-  port: 8080,
-  host: '127.0.0.1'
-}, ()=>{
-  console.log("🚀 Server running at port 8080")
-})
+app.listen(
+  {
+    port: 8080,
+    host: '127.0.0.1',
+  },
+  () => {
+    console.log('🚀 Server running at port 8080')
+  },
+)
 
-app.get('/', (request, reply)=>{
-  reply.send("Worked")
+app.get('/', (request, reply) => {
+  reply.send('Worked')
 })

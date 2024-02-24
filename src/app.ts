8b396062be1878f2a1899a3e60aaa2cddd175fb1
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2024-02-23 21:29:41 -0300

    feat: ✨ start project config

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import {app} from './server'
+
+app.listen({
+  port: 8080,
+  host: '127.0.0.1'
+}, ()=>{
+  console.log("🚀 Server running at port 8080")
+})
+
+app.get('/', (request, reply)=>{
+  reply.send("Worked")
+})

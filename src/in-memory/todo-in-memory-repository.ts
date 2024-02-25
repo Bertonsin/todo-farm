cf6f77edb2349906197340f7dcd500584950dc75
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2024-02-25 18:22:09 -0300

    feat: ✨ add todo in memory repository

---
+++ src/in-memory/todo-in-memory-repository.ts
@@ -0,0 +1,21 @@
+import { Prisma, Todo } from '@prisma/client'
+import { TodoRepository } from '../repositories/todoRepository'
+
+export class InMemoryTodoRepository implements TodoRepository {
+  todos: Todo[] = []
+  create(todo: Prisma.TodoCreateInput) {
+    const id = todo.id ?? 'in-memory-id'
+    this.todos.push({
+      id,
+      created_at: new Date(todo.created_at ?? new Date()),
+      description: todo.description,
+      done: todo.done ?? false,
+      name: todo.name,
+      updated_at: new Date(todo.updated_at ?? new Date()),
+    })
+
+    return {
+      id,
+    }
+  }
+}

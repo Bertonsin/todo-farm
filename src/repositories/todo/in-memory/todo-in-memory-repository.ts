819d274a81ece276df86b93c1fb073de87ff4ced
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2024-05-09 23:55:26 -0300

    feat: ✨ add in memory repository in another location

--- src/repositories/todo/in-memory/todo-in-memory-repository.ts
+++ src/repositories/todo/in-memory/todo-in-memory-repository.ts
@@ -1,9 +1,9 @@
 import { Prisma, Todo } from '@prisma/client'
-import { TodoRepository } from '../repositories/todoRepository'
+import { TodoRepository } from '../../todoRepository'
 
 export class InMemoryTodoRepository implements TodoRepository {
   todos: Todo[] = []
-  create(todo: Prisma.TodoCreateInput) {
+  async create(todo: Prisma.TodoCreateInput) {
     const id = todo.id ?? 'in-memory-id'
     this.todos.push({
       id,
@@ -19,13 +19,13 @@ export class InMemoryTodoRepository implements TodoRepository {
     }
   }
 
-  getList() {
+  async getList() {
     return {
       data: this.todos,
     }
   }
 
-  update(id: string, todo: Prisma.TodoUpdateInput) {
+  async update(id: string, todo: Prisma.TodoUpdateInput) {
     const currentTodo = this.todos.find((item) => item.id === id)
     if (!currentTodo) return null
     for (const iterator in Object(currentTodo)) {

4b0935bebcef3e39cf42496fc03b2f3e839a548a
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2024-05-09 23:56:26 -0300

    feat: ✨ add get todo controller

---
+++ src/controllers/todo/get-todo.ts
@@ -0,0 +1,14 @@
+import { Prisma } from '@prisma/client'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { TodoPrismaRepository } from '../../repositories/todo/prisma'
+import { ViewTodoUseCase } from '../../use-cases/view-todo/view-todo-use-case'
+
+export async function getTodoController(
+  req: FastifyRequest<{ Body: Prisma.TodoCreateInput }>,
+  reply: FastifyReply,
+) {
+  const todoRepository = new TodoPrismaRepository()
+  const useCase = new ViewTodoUseCase(todoRepository)
+  const { data } = await useCase.execute()
+  return reply.send({ data })
+}

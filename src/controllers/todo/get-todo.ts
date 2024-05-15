4a4aa1b1b626b4cd49bc52e4866d04739505a128
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2024-05-14 23:30:02 -0300

    feat: ✨ add errors validations for get todo

--- src/controllers/todo/get-todo.ts
+++ src/controllers/todo/get-todo.ts
@@ -2,13 +2,19 @@ import { Prisma } from '@prisma/client'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { TodoPrismaRepository } from '../../repositories/todo/prisma'
 import { ViewTodoUseCase } from '../../use-cases/view-todo/view-todo-use-case'
+import { ServerTodoError } from '../../errors/todo/serverError'
 
 export async function getTodoController(
   req: FastifyRequest<{ Body: Prisma.TodoCreateInput }>,
   reply: FastifyReply,
 ) {
-  const todoRepository = new TodoPrismaRepository()
-  const useCase = new ViewTodoUseCase(todoRepository)
-  const { data } = await useCase.execute()
-  return reply.send(data)
+  try {
+    const todoRepository = new TodoPrismaRepository()
+    const useCase = new ViewTodoUseCase(todoRepository)
+    const { data } = await useCase.execute()
+    return reply.send(data)
+  } catch (error) {
+    reply.send('Erro no servidor!').status(500)
+    throw new ServerTodoError()
+  }
 }
